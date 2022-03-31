8b2a4ddf9d63535c1d7da94f9dfd9af0fc9b7b07
Dimitar Mirchev <dimitar.mirchev@sap.com>
2022-03-31 11:16:49 +0300

    Do not mutate kube-apiserver on cluster hibernation (#28)
    * Remove configs from kapi server when cluster is hibernated
    
    * Reorder imports
    
    * Deploy config secret only if cluster is not hibernated
    
    * Check for ready apiserver replicas before mutating
    
    * Try to not remove oidc related configurations
    
    * Cleanup

--- pkg/webhook/kapiserver/ensurer.go
+++ pkg/webhook/kapiserver/ensurer.go
@@ -8,19 +8,27 @@ import (
 	"context"
 
 	"github.com/gardener/gardener-extension-shoot-oidc-service/pkg/constants"
+	"github.com/gardener/gardener/extensions/pkg/controller"
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	gcontext "github.com/gardener/gardener/extensions/pkg/webhook/context"
 	"github.com/gardener/gardener/extensions/pkg/webhook/controlplane/genericmutator"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	gutil "github.com/gardener/gardener/pkg/utils/gardener"
+
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+)
 
-	"k8s.io/apimachinery/pkg/types"
+const (
+	oidcWebhookConfigPrefix               = "--authentication-token-webhook-config-file="
+	oidcWebhookCacheTTLPrefix             = "--authentication-token-webhook-cache-ttl="
+	oidcAuthenticatorKubeConfigVolumeName = "oidc-webhook-authenticator-kubeconfig"
+	tokenValidatorSecretVolumeName        = "token-validator-secret"
 )
 
 type ensurer struct {
@@ -42,16 +50,15 @@ func (e *ensurer) EnsureKubeAPIServerDeployment(ctx context.Context, _ gcontext.
 	ps := &template.Spec
 
 	if c := extensionswebhook.ContainerWithName(ps.Containers, v1beta1constants.DeploymentNameKubeAPIServer); c != nil {
+		if new.Status.ReadyReplicas <= 0 {
+			return nil
+		}
+
 		namespacedName := types.NamespacedName{
 			Namespace: new.Namespace,
 			Name:      constants.WebhookKubeConfigSecretName,
 		}
 		secret := &corev1.Secret{}
-
-		var (
-			oidcAuthenticatorKubeConfigVolumeName = "oidc-webhook-authenticator-kubeconfig"
-			tokenValidatorSecretVolumeName        = "token-validator-secret"
-		)
 
 		if err := e.client.Get(ctx, namespacedName, secret); err != nil {
 			if apierrors.IsNotFound(err) {
@@ -61,61 +68,15 @@ func (e *ensurer) EnsureKubeAPIServerDeployment(ctx context.Context, _ gcontext.
 			}
 		}
 
-		c.Command = extensionswebhook.EnsureStringWithPrefix(c.Command, "--authentication-token-webhook-config-file=", "/var/run/secrets/oidc-webhook/authenticator/kubeconfig")
-		c.Command = extensionswebhook.EnsureStringWithPrefix(c.Command, "--authentication-token-webhook-cache-ttl=", "0")
+		cluster, err := controller.GetCluster(ctx, e.client, new.Namespace)
+		if err != nil {
+			return err
+		}
+		if controller.IsHibernated(cluster) {
+			return nil
+		}
 
-		c.VolumeMounts = extensionswebhook.EnsureVolumeMountWithName(c.VolumeMounts, corev1.VolumeMount{
-			Name:      oidcAuthenticatorKubeConfigVolumeName,
-			ReadOnly:  true,
-			MountPath: "/var/run/secrets/oidc-webhook/authenticator",
-		})
-
-		c.VolumeMounts = extensionswebhook.EnsureVolumeMountWithName(c.VolumeMounts, corev1.VolumeMount{
-			Name:      tokenValidatorSecretVolumeName,
-			ReadOnly:  true,
-			MountPath: "/var/run/secrets/oidc-webhook/token-validator",
-		})
-
-		ps.Volumes = extensionswebhook.EnsureVolumeWithName(ps.Volumes, corev1.Volume{
-			Name: oidcAuthenticatorKubeConfigVolumeName,
-			VolumeSource: corev1.VolumeSource{
-				Secret: &corev1.SecretVolumeSource{
-					SecretName: constants.WebhookKubeConfigSecretName,
-				},
-			},
-		})
-
-		ps.Volumes = extensionswebhook.EnsureVolumeWithName(ps.Volumes, corev1.Volume{
-			Name: tokenValidatorSecretVolumeName,
-			VolumeSource: corev1.VolumeSource{
-				Projected: &corev1.ProjectedVolumeSource{
-					DefaultMode: pointer.Int32(420),
-					Sources: []corev1.VolumeProjection{
-						{
-							Secret: &corev1.SecretProjection{
-								Items: []corev1.KeyToPath{
-									{Key: "ca.crt", Path: "ca.crt"},
-								},
-								LocalObjectReference: corev1.LocalObjectReference{
-									Name: v1beta1constants.SecretNameCACluster,
-								},
-							},
-						},
-						{
-							Secret: &corev1.SecretProjection{
-								Items: []corev1.KeyToPath{
-									{Key: "token", Path: "token"},
-								},
-								LocalObjectReference: corev1.LocalObjectReference{
-									Name: gutil.SecretNamePrefixShootAccess + constants.ApplicationName + "-token-validator",
-								},
-							},
-						},
-					},
-				},
-			},
-		})
-
+		ensureKubeAPIServerIsMutated(ps, c)
 	}
 
 	return nil
@@ -126,4 +87,63 @@ func NewEnsurer(logger logr.Logger) genericmutator.Ensurer {
 	return &ensurer{
 		logger: logger.WithName("oidc-controlplane-ensurer"),
 	}
+}
+
+// ensureKubeAPIServerIsMutated ensures that the kube-apiserver deployment is mutated accordingly
+// so that it is able to communicate with the oidc-webhook-authenticator
+func ensureKubeAPIServerIsMutated(ps *corev1.PodSpec, c *corev1.Container) {
+	c.Command = extensionswebhook.EnsureStringWithPrefix(c.Command, oidcWebhookConfigPrefix, "/var/run/secrets/oidc-webhook/authenticator/kubeconfig")
+	c.Command = extensionswebhook.EnsureStringWithPrefix(c.Command, oidcWebhookCacheTTLPrefix, "0")
+
+	c.VolumeMounts = extensionswebhook.EnsureVolumeMountWithName(c.VolumeMounts, corev1.VolumeMount{
+		Name:      oidcAuthenticatorKubeConfigVolumeName,
+		ReadOnly:  true,
+		MountPath: "/var/run/secrets/oidc-webhook/authenticator",
+	})
+
+	c.VolumeMounts = extensionswebhook.EnsureVolumeMountWithName(c.VolumeMounts, corev1.VolumeMount{
+		Name:      tokenValidatorSecretVolumeName,
+		ReadOnly:  true,
+		MountPath: "/var/run/secrets/oidc-webhook/token-validator",
+	})
+
+	ps.Volumes = extensionswebhook.EnsureVolumeWithName(ps.Volumes, corev1.Volume{
+		Name: oidcAuthenticatorKubeConfigVolumeName,
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName: constants.WebhookKubeConfigSecretName,
+			},
+		},
+	})
+
+	ps.Volumes = extensionswebhook.EnsureVolumeWithName(ps.Volumes, corev1.Volume{
+		Name: tokenValidatorSecretVolumeName,
+		VolumeSource: corev1.VolumeSource{
+			Projected: &corev1.ProjectedVolumeSource{
+				DefaultMode: pointer.Int32(420),
+				Sources: []corev1.VolumeProjection{
+					{
+						Secret: &corev1.SecretProjection{
+							Items: []corev1.KeyToPath{
+								{Key: "ca.crt", Path: "ca.crt"},
+							},
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: v1beta1constants.SecretNameCACluster,
+							},
+						},
+					},
+					{
+						Secret: &corev1.SecretProjection{
+							Items: []corev1.KeyToPath{
+								{Key: "token", Path: "token"},
+							},
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: gutil.SecretNamePrefixShootAccess + constants.ApplicationName + "-token-validator",
+							},
+						},
+					},
+				},
+			},
+		},
+	})
 }
--- pkg/webhook/kapiserver/mutator_test.go
+++ pkg/webhook/kapiserver/mutator_test.go
@@ -6,21 +6,31 @@ package kapiserver
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
 	"github.com/gardener/gardener-extension-shoot-oidc-service/pkg/constants"
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	"github.com/gardener/gardener/extensions/pkg/webhook/controlplane/genericmutator"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	mockclient "github.com/gardener/gardener/pkg/mock/controller-runtime/client"
+	gutil "github.com/gardener/gardener/pkg/utils/gardener"
+
 	"github.com/golang/mock/gomock"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
 )
 
@@ -36,6 +46,55 @@ var _ = Describe("Mutator", func() {
 		ctrl *gomock.Controller
 		ctx  = context.Background()
 
+		oidcAuthenticatorKubeConfigVolumeMount = corev1.VolumeMount{
+			Name:      oidcAuthenticatorKubeConfigVolumeName,
+			ReadOnly:  true,
+			MountPath: "/var/run/secrets/oidc-webhook/authenticator",
+		}
+		oidcAuthenticatorKubeConfigVolume = corev1.Volume{
+			Name: oidcAuthenticatorKubeConfigVolumeName,
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: constants.WebhookKubeConfigSecretName,
+				},
+			},
+		}
+		tokenValidatorSecretVolumeMount = corev1.VolumeMount{
+			Name:      tokenValidatorSecretVolumeName,
+			ReadOnly:  true,
+			MountPath: "/var/run/secrets/oidc-webhook/token-validator",
+		}
+		tokenValidatorSecretVolume = corev1.Volume{
+			Name: tokenValidatorSecretVolumeName,
+			VolumeSource: corev1.VolumeSource{
+				Projected: &corev1.ProjectedVolumeSource{
+					DefaultMode: pointer.Int32(420),
+					Sources: []corev1.VolumeProjection{
+						{
+							Secret: &corev1.SecretProjection{
+								Items: []corev1.KeyToPath{
+									{Key: "ca.crt", Path: "ca.crt"},
+								},
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: v1beta1constants.SecretNameCACluster,
+								},
+							},
+						},
+						{
+							Secret: &corev1.SecretProjection{
+								Items: []corev1.KeyToPath{
+									{Key: "token", Path: "token"},
+								},
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: gutil.SecretNamePrefixShootAccess + constants.ApplicationName + "-token-validator",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
 		checkDeploymentIsCorrectlyMutated = func(deployment *appsv1.Deployment) {
 			// Check that the kube-apiserver container still exists
 			c := extensionswebhook.ContainerWithName(deployment.Spec.Template.Spec.Containers, v1beta1constants.DeploymentNameKubeAPIServer)
@@ -43,14 +102,78 @@ var _ = Describe("Mutator", func() {
 
 			Expect(c.Command).To(ContainElement(cacheElement))
 			Expect(c.Command).To(ContainElement(configElement))
+
+			Expect(c.VolumeMounts).To(ContainElement(oidcAuthenticatorKubeConfigVolumeMount))
+			Expect(deployment.Spec.Template.Spec.Volumes).To(ContainElement(oidcAuthenticatorKubeConfigVolume))
+
+			Expect(c.VolumeMounts).To(ContainElement(tokenValidatorSecretVolumeMount))
+			Expect(deployment.Spec.Template.Spec.Volumes).To(ContainElement(tokenValidatorSecretVolume))
 		}
 		checkDeploymentIsNotMutated = func(deployment *appsv1.Deployment) {
 			// Check that the kube-apiserver container still exists
 			c := extensionswebhook.ContainerWithName(deployment.Spec.Template.Spec.Containers, v1beta1constants.DeploymentNameKubeAPIServer)
 			Expect(c).To(Not(BeNil()))
 
-			Expect(c.Command).NotTo(ContainElement(cacheElement))
-			Expect(c.Command).NotTo(ContainElement(configElement))
+			for _, v := range c.Command {
+				Expect(strings.HasPrefix(v, oidcWebhookConfigPrefix)).To(BeFalse())
+				Expect(strings.HasPrefix(v, oidcWebhookCacheTTLPrefix)).To(BeFalse())
+			}
+
+			for _, v := range c.VolumeMounts {
+				Expect(v.Name).NotTo(Equal(oidcAuthenticatorKubeConfigVolumeName))
+				Expect(v.Name).NotTo(Equal(tokenValidatorSecretVolumeName))
+			}
+
+			for _, v := range deployment.Spec.Template.Spec.Volumes {
+				Expect(v.Name).NotTo(Equal(oidcAuthenticatorKubeConfigVolumeName))
+				Expect(v.Name).NotTo(Equal(tokenValidatorSecretVolumeName))
+
+				// Ensure no volume source with the oidc config related secrets are present
+				if v.VolumeSource.Secret != nil {
+					Expect(v.VolumeSource.Secret.SecretName).NotTo(Equal(constants.WebhookKubeConfigSecretName))
+				}
+
+				if v.VolumeSource.Projected != nil && len(v.VolumeSource.Projected.Sources) > 0 {
+					for _, s := range v.VolumeSource.Projected.Sources {
+						if s.Secret != nil {
+							Expect(s.Secret.LocalObjectReference.Name).NotTo(Equal(gutil.SecretNamePrefixShootAccess + constants.ApplicationName + "-token-validator"))
+						}
+					}
+				}
+			}
+		}
+
+		encode = func(obj runtime.Object) []byte {
+			bytes, err := json.Marshal(obj)
+			Expect(err).NotTo(HaveOccurred())
+			return bytes
+		}
+
+		getCluster = func(hibernated bool) interface{} {
+			return func(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+				*obj.(*extensionsv1alpha1.Cluster) = extensionsv1alpha1.Cluster{
+					Spec: extensionsv1alpha1.ClusterSpec{
+						Shoot: runtime.RawExtension{
+							Raw: encode(&gardencorev1beta1.Shoot{
+								TypeMeta: metav1.TypeMeta{
+									Kind:       "Shoot",
+									APIVersion: "core.gardener.cloud/v1beta1",
+								},
+								ObjectMeta: metav1.ObjectMeta{
+									Name: "some-cluster",
+								},
+								Spec: gardencorev1beta1.ShootSpec{
+									Hibernation: &gardencorev1beta1.Hibernation{
+										Enabled: &hibernated,
+									},
+								},
+							}),
+						},
+					},
+				}
+
+				return nil
+			}
 		}
 	)
 
@@ -71,6 +194,10 @@ var _ = Describe("Mutator", func() {
 				Namespace: namespace,
 				Name:      constants.WebhookKubeConfigSecretName,
 			}
+			clusterNamespacedName = types.NamespacedName{
+				// cluster is named after the namespace
+				Name: "test",
+			}
 			errNotFound = &apierrors.StatusError{ErrStatus: metav1.Status{Reason: metav1.StatusReasonNotFound}}
 			errInternal = fmt.Errorf("internal error")
 		)
@@ -89,6 +216,9 @@ var _ = Describe("Mutator", func() {
 						},
 					},
 				},
+				Status: appsv1.DeploymentStatus{
+					ReadyReplicas: 1,
+				},
 			}
 
 			client = mockclient.NewMockClient(ctrl)
@@ -100,6 +230,7 @@ var _ = Describe("Mutator", func() {
 
 		It("should add missing flags to a kube-apiserver pod", func() {
 			client.EXPECT().Get(ctx, secretNamespacedName, gomock.AssignableToTypeOf(&corev1.Secret{})).Return(nil)
+			client.EXPECT().Get(ctx, clusterNamespacedName, &extensionsv1alpha1.Cluster{}).DoAndReturn(getCluster(false))
 			err := ensurer.EnsureKubeAPIServerDeployment(ctx, nil, deployment, nil)
 			Expect(err).NotTo(HaveOccurred())
 			checkDeploymentIsCorrectlyMutated(deployment)
@@ -112,19 +243,35 @@ var _ = Describe("Mutator", func() {
 			}
 
 			client.EXPECT().Get(ctx, secretNamespacedName, gomock.AssignableToTypeOf(&corev1.Secret{})).Return(nil)
+			client.EXPECT().Get(ctx, clusterNamespacedName, &extensionsv1alpha1.Cluster{}).DoAndReturn(getCluster(false))
 			err := ensurer.EnsureKubeAPIServerDeployment(ctx, nil, deployment, nil)
 			Expect(err).NotTo(HaveOccurred())
 			checkDeploymentIsCorrectlyMutated(deployment)
 		})
 
-		It("should not add flags to a kube-apiserver pod if webhook secret does not exist", func() {
+		It("should not add oidc configs to a kube-apiserver pod if kube-apiserver deployment does not have ready replicas", func() {
+			deployment.Status.ReadyReplicas = 0
+			err := ensurer.EnsureKubeAPIServerDeployment(ctx, nil, deployment, nil)
+			Expect(err).NotTo(HaveOccurred())
+			checkDeploymentIsNotMutated(deployment)
+		})
+
+		It("should not add oidc configs to a kube-apiserver pod if webhook secret does not exist", func() {
 			client.EXPECT().Get(ctx, secretNamespacedName, gomock.AssignableToTypeOf(&corev1.Secret{})).Return(errNotFound)
 			err := ensurer.EnsureKubeAPIServerDeployment(ctx, nil, deployment, nil)
 			Expect(err).NotTo(HaveOccurred())
 			checkDeploymentIsNotMutated(deployment)
 		})
 
-		It("should not add flags to a kube-apiserver pod and return error if fails to get webhook secret and error is different from not found", func() {
+		It("should not add oidc configs to a kube-apiserver pod if webhook secret exists but cluster is hibernated", func() {
+			client.EXPECT().Get(ctx, secretNamespacedName, gomock.AssignableToTypeOf(&corev1.Secret{})).Return(nil)
+			client.EXPECT().Get(ctx, clusterNamespacedName, &extensionsv1alpha1.Cluster{}).DoAndReturn(getCluster(true))
+			err := ensurer.EnsureKubeAPIServerDeployment(ctx, nil, deployment, nil)
+			Expect(err).NotTo(HaveOccurred())
+			checkDeploymentIsNotMutated(deployment)
+		})
+
+		It("should not add oidc configs to a kube-apiserver pod and return error if fails to get webhook secret and error is different from not found", func() {
 			client.EXPECT().Get(ctx, secretNamespacedName, gomock.AssignableToTypeOf(&corev1.Secret{})).Return(errInternal)
 			err := ensurer.EnsureKubeAPIServerDeployment(ctx, nil, deployment, nil)
 			Expect(err).To(HaveOccurred())
