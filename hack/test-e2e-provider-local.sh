1e5b6c78674b01b15e3dd16f3be0f650eda1cd6d
Dimitar Mirchev <dimitar.mirchev@sap.com>
2022-04-11 11:23:56 +0300

    Add a dummy step to include in prow ci

---
+++ hack/test-e2e-provider-local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+echo "This is a dummy step"
