accd6a18ffc3977a74c69b693710dcdec2f67758
Sonu Kumar Singh <sksgkpvks@gmail.com>
2023-10-18 15:25:16 +0530

    Implement ForceDelete

--- pkg/controller/lifecycle/actuator.go
+++ pkg/controller/lifecycle/actuator.go
@@ -276,6 +276,11 @@ func (a *actuator) deleteSecret(ctx context.Context, name, namespace string) err
 	}))
 }
 
+// ForceDelete the Extension resource.
+func (a *actuator) ForceDelete(ctx context.Context, log logr.Logger, ex *extensionsv1alpha1.Extension) error {
+	return a.Delete(ctx, log, ex)
+}
+
 // Restore the Extension resource.
 func (a *actuator) Restore(ctx context.Context, log logr.Logger, ex *extensionsv1alpha1.Extension) error {
 	return a.Reconcile(ctx, log, ex)
