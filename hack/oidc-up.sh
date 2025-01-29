650a7307f27cf2eda237fdc27a7fddebf2297762
vpnachev <vladimir.nachev@sap.com>
2025-01-29 17:50:53 +0200

    Apply review feedback 2

--- hack/oidc-up.sh
+++ hack/oidc-up.sh
@@ -19,6 +19,11 @@ cp -f $repo_root/example/controller-registration.yaml $repo_root/tmp/controller-
 yq -i e "(select (.helm.values.image) | .helm.values.image.tag) |= \"$version\"" $repo_root/tmp/controller-registration.yaml
 yq -i e '(select (.helm.values.image) | .helm.values.image.repository) |= "docker.io/library/shoot-oidc-service-local"' $repo_root/tmp/controller-registration.yaml
 
+# --server-side apply is a workaround for https://github.com/gardener/gardener/issues/10267.
+# kubectl apply attempts a strategic merge patch which fails for a ControllerDeployment.
+# For more details, see https://github.com/gardener/gardener/issues/10267.
+#
+# TODO: Remove `--server-side` and `--force-conflicts` flags when the above issue is resolved.
 kubectl apply -f "$repo_root/tmp/controller-registration.yaml" \
     --server-side \
     --force-conflicts
